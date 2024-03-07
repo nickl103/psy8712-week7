296e1331e5e30254429fdb43541a14d5aab512dc
nickl103 <nickl103@umn.edu>
2024-03-07 17:48:32 -0600

    making and saving figures

--- R/week7.R
+++ R/week7.R
@@ -13,3 +13,48 @@ week7_tbl <- read_csv("../data/week3.csv") %>%
   filter(q6 == 1) %>%
   select(-q6) %>%
   mutate(timeSpent= timeEnd-timeStart)
+
+
+
+
+
+#Visualization
+week7_tbl%>%
+  select(q1:q10) %>%
+  ggpairs
+(week7_tbl %>%
+    ggplot(aes(timeStart, q1)) +
+    geom_point() +
+    labs(x= "Date of Experiment", y= "Q1 Score")) %>%
+  ggsave(filename="../figs/fig1.png")
+
+
+
+(week7_tbl %>% 
+    ggplot(aes(q1,q2, color=gender)) +
+    geom_jitter() +
+    labs(x="q1",y="q2", color="Participant Gender")) %>%
+  ggsave(filename="../figs/fig2.png")
+
+(week7_tbl %>% 
+    ggplot(aes(q1,q2,)) +
+    geom_jitter() +
+    facet_grid(col=vars(gender),scales="free_x")) %>%
+  ggsave(filename="../figs/fig3.png")
+
+
+
+(week7_tbl %>%
+    ggplot(aes(gender,timeSpent)) +
+    geom_boxplot() +
+    labs(x="Gender", y="Time Elapsed(mins)")) %>%
+  ggsave(filename="../figs/fig4.png")
+
+
+(week7_tbl %>%
+    ggplot(aes(q5,q7, color=condition)) +
+    geom_jitter() +
+    geom_smooth(method="lm",se=F) +
+    labs(x="Score on Q5", y="Score on Q7", color="Experimental Condition") +
+    theme(legend.position= "bottom", legend.background= element_rect(fill = "#E0E0E0"))) %>%
+  ggsave(filenam="../figs/fig5.png")
